deb51a4c95788a755f156cc2e8ffb6039296d2d7
Afroze Shaik <afroze.shaik@supermercato24.it>
2022-04-19 02:24:53 +0530

    feat (lumen all): add first basic installation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# workhours
+# Workhours
+
+This api helps organize workhours of employees
+
+## Framework
+Build using [Lumen](https://github.com/laravel/lumen) on top of PHP 7.4
+
+## Requirements
+- Bash
+- Docker
+- Docker compose
+
+_Linux systems are prefered_
+
+## Architecture 
+- Docker containers
+  - PHP 7.4
+  - nginx (server)
+  - mysql 5.7
---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+
+/**
+ * Class TestCase.
+ */
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Laravel\Lumen\Application
+     */
+    public function createApplication()
+    {
+        return require __DIR__ . '/../bootstrap/app.php';
+    }
+}
