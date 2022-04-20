714b23cfc16ecfbabd0c82dcac52cff2977f7cae
Afroze Shaik <afroze.shaik@supermercato24.it>
2022-04-20 19:12:53 +0530

    feat (controllers): create employees

---
+++ app/Http/Controllers/Authenticated/v1/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Authenticated\v1;
+
+use App\Http\Controllers\Controller as BaseController;
+
+/**
+ * Class Controller.
+ */
+class Controller extends BaseController
+{
+}
